9a327ba87b5175f4a89d50b2423b2f75aa6e57ec
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 01:03:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,21 @@
 def new_hash
-
+empty_hash = {}
 end
 
 def actor
-
+fav_actor = {
+  :name => "Dwayne The Rock Johnson"
+}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  :railraods => {}
+	}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
